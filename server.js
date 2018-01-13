fe084c1fe3e47e1a0a3ad91aed57605351448b0b
mohan <mohankukreja1@gmail.com>
2018-01-13 13:45:35 +0530

    register page
    Signed-off-by: mohan <mohankukreja1@gmail.com>

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app=express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 
-
+app.use('/',express.static(path.join(__dirname,'frontend')))
 
 
 app.listen(2222,()=>{
